ae6a922d7fcbe7bb05004b8656a71307dfe62585
bliutvikler <post@bliutvikler.no>
2024-07-14 09:19:34 +0200

    Only the owner or participant of the board can change swimlanes

--- src/main/java/com/bliutvikler/bliutvikler/swimlane/controller/SwimlaneController.java
+++ src/main/java/com/bliutvikler/bliutvikler/swimlane/controller/SwimlaneController.java
@@ -1,11 +1,15 @@
 package com.bliutvikler.bliutvikler.swimlane.controller;
 import com.bliutvikler.bliutvikler.swimlane.model.Swimlane;
 import com.bliutvikler.bliutvikler.swimlane.service.SwimlaneService;
+import com.bliutvikler.bliutvikler.user.model.User;
+import com.bliutvikler.bliutvikler.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,10 +22,17 @@ public class SwimlaneController {
     @Autowired
     private SwimlaneService swimlaneService;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/create/{boardId}")
     public ResponseEntity<Swimlane> createSwimlane(@PathVariable Long boardId) {
         try {
-            Swimlane savedSwimlane = swimlaneService.createNewSwimlane(boardId);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String username = authentication.getName();
+            User currentUser = userService.findByUsername(username); // get userobject from the database
+
+            Swimlane savedSwimlane = swimlaneService.createNewSwimlane(boardId, currentUser);
             logger.info("Swimlane created successfully with id -  {}", savedSwimlane.getId());
             return ResponseEntity.ok(savedSwimlane);
         } catch (IllegalStateException e) {
@@ -37,7 +48,11 @@ public class SwimlaneController {
     @DeleteMapping("/delete/{swimlaneId}/{boardId}")
     public ResponseEntity<Swimlane> deleteSwimlane(@PathVariable Long swimlaneId, @PathVariable Long boardId) {
         try {
-            swimlaneService.deleteExistingSwimlane(swimlaneId, boardId);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String username = authentication.getName();
+            User currentUser = userService.findByUsername(username); // get userobject from the database
+
+            swimlaneService.deleteExistingSwimlane(swimlaneId, boardId, currentUser);
             logger.info("Swimlane deleted successfully with ID -  {}", swimlaneId);
             return ResponseEntity.ok().build(); // ingen body returneres pga delete
         } catch (IllegalStateException e) {
@@ -53,7 +68,11 @@ public class SwimlaneController {
     @PutMapping("update/{swimlaneId}")
     public ResponseEntity<Swimlane> updateSwimlaneName(@PathVariable Long swimlaneId, @RequestBody Swimlane newSwimlaneName) {
         try {
-            Swimlane swimlaneToUpdate = swimlaneService.updateSwimlaneName(swimlaneId, newSwimlaneName);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String username = authentication.getName();
+            User currentUser = userService.findByUsername(username); // get userobject from the database
+
+            Swimlane swimlaneToUpdate = swimlaneService.updateSwimlaneName(swimlaneId, newSwimlaneName, currentUser);
             logger.info("Updated swimlane name successfully");
             return ResponseEntity.ok(swimlaneToUpdate);
         } catch (Exception e) {
--- src/main/java/com/bliutvikler/bliutvikler/swimlane/service/SwimlaneService.java
+++ src/main/java/com/bliutvikler/bliutvikler/swimlane/service/SwimlaneService.java
@@ -4,6 +4,7 @@ import com.bliutvikler.bliutvikler.board.model.Board;
 import com.bliutvikler.bliutvikler.board.repository.BoardRepository;
 import com.bliutvikler.bliutvikler.swimlane.model.Swimlane;
 import com.bliutvikler.bliutvikler.swimlane.repository.SwimlaneRepository;
+import com.bliutvikler.bliutvikler.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,13 +23,21 @@ public class SwimlaneService {
 
     private static final int MAX_SWIMLANES = 10;
 
-    public Swimlane createNewSwimlane(Long boardId) {
+    public Swimlane createNewSwimlane(Long boardId, User currentUser) {
         // Finne tilhørende board og sjekk om bord finnes
         Optional<Board> boardOptional = boardRepository.findById(boardId);
         if (!boardOptional.isPresent()) {
             throw new IllegalStateException("Board not found");
         }
         Board board = boardOptional.get();
+
+        Boolean isOwner = board.getOwner().getId().equals(currentUser.getId());
+        Boolean isParticipant = board.getParticipants().stream().anyMatch(participant -> participant.getId().equals(currentUser.getId()));
+
+        if (!isOwner && !isParticipant) {
+            throw new IllegalStateException("User is not authorized to create swimlanes in this board");
+        }
+
         // Finne tilhørende swimlane
         List<Swimlane> swimlanes = board.getSwimlanes();
 
@@ -47,10 +56,17 @@ public class SwimlaneService {
         return swimlaneRepository.save(newDefaultSwimlane);
     }
 
-    public void deleteExistingSwimlane(Long swimlaneId, Long boardId) {
+    public void deleteExistingSwimlane(Long swimlaneId, Long boardId, User currentUser) {
         Swimlane swimlaneToDelete = swimlaneRepository.findById(swimlaneId).orElseThrow(() -> new IllegalArgumentException("Swimlane not found with ID: " + swimlaneId));
         Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("Cannot find board with ID: " + boardId));
         List<Swimlane> swimlanes = board.getSwimlanes();
+
+        Boolean isOwner = board.getOwner().getId().equals(currentUser.getId());
+        Boolean isParticipant = board.getParticipants().stream().anyMatch(participant -> participant.getId().equals(currentUser.getId()));
+
+        if (!isOwner && !isParticipant) {
+            throw new IllegalStateException("User is not authorized to delete swimlanes in this board");
+        }
 
         if (swimlanes.size() < 3) {
             throw new IllegalStateException("Too few swimlanes to use delete operation. Minimum 2 swimlanes must be present.");
@@ -63,8 +79,17 @@ public class SwimlaneService {
         swimlaneRepository.delete(swimlaneToDelete);
     }
 
-    public Swimlane updateSwimlaneName(Long swimlaneId, Swimlane swimLaneInputData) {
+    public Swimlane updateSwimlaneName(Long swimlaneId, Swimlane swimLaneInputData, User currentUser) {
         Swimlane swimlaneToUpdate = swimlaneRepository.findById(swimlaneId).orElseThrow(() -> new IllegalArgumentException("Swimlane not found with ID: " + swimlaneId));
+
+        Board board = swimlaneToUpdate.getBoard();
+
+        Boolean isOwner = board.getOwner().getId().equals(currentUser.getId());
+        Boolean isParticipant = board.getParticipants().stream().anyMatch(participant -> participant.getId().equals(currentUser.getId()));
+
+        if (!isOwner && !isParticipant) {
+            throw new IllegalStateException("User is not authorized to update swimlanes in this board");
+        }
 
         swimlaneToUpdate.setName(swimLaneInputData.getName());
         return swimlaneRepository.save(swimlaneToUpdate);
