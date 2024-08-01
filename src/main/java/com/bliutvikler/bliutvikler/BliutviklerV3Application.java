17ed413a4b81cad15cd2e9086afb387cfdc13740
bliutvikler <post@bliutvikler.no>
2024-08-01 20:40:39 +0200

    Test database connection with new env variables

--- src/main/java/com/bliutvikler/bliutvikler/BliutviklerV3Application.java
+++ src/main/java/com/bliutvikler/bliutvikler/BliutviklerV3Application.java
@@ -1,15 +1,25 @@
 package com.bliutvikler.bliutvikler;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
 
 
 @SpringBootApplication
 public class BliutviklerV3Application {
 
-	@Value("${jwt.secret}")
+	@Autowired
+	private Environment environment;
+
+	private String datasourceUrl;
+	private String datasourceUsername;
+	private String datasourcePassword;
+
+	/*@Value("${jwt.secret}")
 	private String jwtSecret;
 
 	@Value("${spring.datasource.url}")
@@ -18,15 +28,26 @@ public class BliutviklerV3Application {
 	@Value("${spring.datasource.username}")
 	private String datasourceUsername;
 
-/*	@Value("${MYSQLPORT}")
+	@Value("${MYSQLPORT}")
 	private String mysqlPort;*/
 
-	@PostConstruct
+	/*@PostConstruct
 	public void logDatabaseProperties() {
 		System.out.println("JWT secret URL: " + jwtSecret);
 		System.out.println("Datasource URL: " + datasourceUrl);
 		System.out.println("Datasource Username: " + datasourceUsername);
 		// System.out.println("MYSQL Port: " + mysqlPort);
+	}*/
+
+	@PostConstruct
+	public void logDatabaseProperties() {
+		datasourceUrl = System.getenv("DB_URL");
+		datasourceUsername = System.getenv("DB_USERNAME");
+		datasourcePassword = System.getenv("DB_PASSWORD");
+
+		System.out.println("Datasource URL: " + datasourceUrl);
+		System.out.println("Datasource Username: " + datasourceUsername);
+		System.out.println("Datasource Password: " + datasourcePassword);
 	}
 
 	@PostConstruct
